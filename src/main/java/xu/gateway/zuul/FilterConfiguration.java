d398f8341a765a2a7c74c297bacab93d428e9f2f
yujun.xu <yujun.xu@linkflowtech.com>
2022-03-07 16:10:08 +0800

    finish configuration zuul

--- src/main/java/xu/gateway/zuul/FilterConfiguration.java
+++ src/main/java/xu/gateway/zuul/FilterConfiguration.java
@@ -1,7 +1,14 @@
 package xu.gateway.zuul;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import xu.gateway.zuul.filter.TokenFilter;
 
 @Configuration
 public class FilterConfiguration {
+
+    @Bean
+    public TokenFilter tokenFilter() {
+        return new TokenFilter();
+    }
 }
